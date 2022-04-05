5c67c58b9b3dfd70c902bb03e912524dee903613
Guilherme Ferreira dos Santos <guiferrera@brq.com>
2022-04-05 20:03:18 -0300

    Add about session

--- pages/sections/about/about.page.js
+++ pages/sections/about/about.page.js
@@ -1,32 +1,46 @@
-import { WrapperAbout, ContentAbout } from "./styles";
+import Image from "next/image";
+
+import perfil from "../../../assets/images/perfil_1.png";
+import { WrapperAbout, ContentAbout, ContentImage } from "./styles";
 
 export default function About() {
   return (
     <WrapperAbout>
+      <h1>
+        SOBRE <span>MIM</span>
+      </h1>
       <ContentAbout>
         <div>
-          <p>SOBRE</p>
+          <p>
+            Olá, prazer em conhecer você!
+            <br />
+            <br />
+            Me chamo Guilherme e sou desenvolvedor web
+            <br />
+            <span>
+              {"["}React.js, React Native, Next.js, Angular, Node.js{"]"}
+            </span>
+            <br />
+            <br />
+            Desde 2017, venho estudando e aplicando todo o conhecimento
+            <br />
+            adquirido em construções de aplicações web e mobile no meu
+            cotidiano.
+            <br />
+            <br />
+            Trago comigo não apenas as experiências de grandes <br />
+            projetos e seus mais diversos segmentos, mas também <br />
+            carinho por cada linha de código.
+          </p>
         </div>
-        <p>
-          Olá, prazer em conhecer você!
-          <br />
-          <br />
-          Me chamo Guilherme e sou desenvolvedor web
-          <br />
-          <span>
-            {"["}React.js, React Native, Next.js, Angular, Node.js{"]"}
-          </span>
-          <br />
-          <br />
-          Desde 2017, venho estudando e aplicando todo o conhecimento
-          <br />
-          adquirido em construções de aplicações web e mobile no meu cotidiano.
-          <br />
-          <br />
-          Trago comigo não apenas as experiências de grandes <br />
-          projetos e seus mais diversos segmentos, mas também <br />
-          carinho por cada linha de código.
-        </p>
+        <ContentImage>
+          <Image
+            className="perfil"
+            src={perfil}
+            alt="Guilherme Santos"
+            unsized={true}
+          />
+        </ContentImage>
       </ContentAbout>
     </WrapperAbout>
   );
--- pages/sections/about/styles.js
+++ pages/sections/about/styles.js
@@ -2,7 +2,65 @@ import styled from "styled-components";
 
 export const WrapperAbout = styled.section`
   width: 100%;
-  border: 1px solid red;
+  background-color: rgba(10, 10, 10, 0.9);
+  padding: 80px 100px;
+
+  h1 {
+    font-family: "Barlow Condensed", sans-serif;
+    text-align: center;
+    letter-spacing: 0.2rem;
+    font-weight: 500;
+    font-size: 2rem;
+    color: white;
+    margin-bottom: 40px;
+
+    span {
+      font-family: "Barlow Condensed", sans-serif;
+      color: #0093e9;
+    }
+  }
 `;
 
-export const ContentAbout = styled.div``;
+export const ContentAbout = styled.div`
+  position: relative;
+  display: flex;
+  color: white;
+
+  p,
+  span {
+    font-family: "Barlow Condensed", sans-serif;
+    font-size: 1.5rem;
+    font-weight: 300;
+    position: relative;
+    z-index: 2;
+  }
+
+  span {
+    color: #0093e9;
+  }
+
+  .perfil {
+    min-width: 300px !important;
+    min-height: 300px !important;
+    object-fit: cover;
+    overflow: hidden;
+    border-radius: 50%;
+  }
+
+  &:before {
+    content: "SOBRE";
+    position: absolute;
+    left: 0;
+    top: 0;
+    margin: -5% 0 0 -5%;
+    z-index: 1;
+    font-family: "Barlow Condensed", sans-serif;
+    font-weight: 500;
+    font-size: 20rem;
+    color: rgba(55, 55, 55, 0.4);
+  }
+`;
+
+export const ContentImage = styled.div`
+  text-align: center;
+`;
